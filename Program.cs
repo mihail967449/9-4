2ce896d9e2777d2adb4db0abbe9bc4c3e3e4c854
ИИП ТТ <kab31lab@gmail.com>
2023-03-20 08:55:31 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 В массиве записана масса каждого из n предметов, загружаемых в автомобиль. Определить общую массу груза.
+            int[] mas = new int[10];
+            Random rnd = new Random();
+            int sum = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rnd.Next(1, 10);
+                sum += mas[i];
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
